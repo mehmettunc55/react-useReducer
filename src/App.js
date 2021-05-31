7718f14fedf4776ceb66339d0bba238ff9bc07a2
mehmettunc55 <mtunc551980@gmail.com>
2021-05-31 16:37:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useReducer } from 'react'
+import { reducer } from './reducer'
+
+const initialState = {
+  data: '',
+  loading: false,
+  error: ''
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const {data, loading, error} = state;
+
+  // const [data, setData] = useState('')
+  // const [loading, setLoading] = useState(false)
+  // const [error, setError] = useState('')
+
+  const fetchData = () => {
+    dispatch({type: 'FETCH_START'});
+
+    fetch('https://api.thecatapi.com/v1/images/search')
+      .then((res) => res.json())
+      .then((res) => {
+        dispatch({type: 'FETCH_SUCCESS', payload: res[0].url})
+      })
+      .catch(() => {
+        dispatch({type: 'FETCH_FAIL', payload: 'Something went wrong!'})
+      })
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <button onClick={fetchData} disabled={loading} style={{ width: '100px', margin: '1rem' }}>
+        Fetch Data
+      </button>
+      {data && <img src={data} alt="cat-img" />}
+      {error && <p>{error}</p>}
+    </div>
+  )
+}
+
+export default App
